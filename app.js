58c5649efd335d82633ce767c5701d64eb1863bd
youcancrafthub <118652138+youcancrafthub@users.noreply.github.com>
2026-07-06 09:57:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 <script src="js/products.js"></script>
 <script src="js/app.js"></script>
+function addToCart(id) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const product = products.find(p => p.id === id);
+
+    const existing = cart.find(item => item.id === id);
+
+    if (existing) {
+        existing.quantity++;
+    } else {
+        cart.push({
+            ...product,
+            quantity: 1
+        });
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert("Product added to cart!");
+}
